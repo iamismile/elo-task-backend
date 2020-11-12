66c76f6f5560bc4ec8dd050b6e7b55c696fa55fc
Ismile <iamismile.dev@gmail.com>
2020-11-12 16:24:38 +0600

    Initail Server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const app = require("./app");
+
+const server = http.createServer(app);
+
+const port = config.PORT;
+server.listen(port, () => {
+  logger.info(`🌟 Server is running on port ${port}`);
+});
